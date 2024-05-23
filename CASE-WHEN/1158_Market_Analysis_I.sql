43144fb28849de651c3f47f8d531c320ba585cf5
amlanpatnaik <48393502+amlanpatnaik@users.noreply.github.com>
2024-05-23 11:07:33 -0700

    Update 1158_Market_Analysis_I.sql

--- CASE-WHEN/1158_Market_Analysis_I.sql
+++ CASE-WHEN/1158_Market_Analysis_I.sql
@@ -4,3 +4,19 @@ FROM Users u
 LEFT JOIN Orders o
 ON u.user_id = o.buyer_id
 GROUP BY u.user_id, u.join_date;
+
+# Solution-2
+
+
+SELECT u.user_id AS buyer_id,
+       u.join_date,
+       IFNULL(o.orders_in_2019, 0) AS orders_in_2019
+FROM users u
+LEFT JOIN
+    (SELECT buyer_id,
+        COUNT(order_id) AS orders_in_2019
+    FROM orders
+    WHERE YEAR(order_date) = 2019
+    GROUP BY 1) AS o
+ON u.user_id = o.buyer_id 
+
